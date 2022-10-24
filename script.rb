d4b492d4858acc93f408d685f999eb62cb30b8be
aleksandar-code <aca.jevdjevic@gmail.com>
2022-10-24 12:58:44 +0200

    Code formatting

--- script.rb
+++ script.rb
@@ -1,34 +1,35 @@
-class Game
+# frozen_string_literal: true
 
+# Game
+class Game
   def initialize
     @board = [
-        [1, 2, 3],
-        [4, 5, 6],
-        [7, 8, 9]
+      [1, 2, 3],
+      [4, 5, 6],
+      [7, 8, 9]
     ]
     @players = [Player.new(0), Player.new(1)]
   end
   attr_accessor :board, :players
 
-  
   def play_game
     id = 0
-    print_board    
+    print_board
     loop do
       loc = @players[id].player_play
       add_moves(id, loc)
       place_at(loc, player_form(id))
       print_board
       return if check_win(id)
+
       id = player_switch(id)
     end
-    
   end
 
   def place_at(loc, form)
-   @board = @board.each do |x|
+    @board = @board.each do |x|
       x.each_with_index do |y, idx|
-       x[idx] = form if y == loc
+        x[idx] = form if y == loc
       end
     end
   end
@@ -45,28 +46,21 @@ class Game
   end
 
   def player_switch(id)
-    return 1 if id == 0
-    return 0
+    return 1 if id.zero?
+
+    0
   end
 
   def player_form(id)
-    return 'X' if id == 0
-    return 'O'
+    return 'X' if id.zero?
+
+    'O'
   end
 
   def check_win(id)
-    moves = @players[id].valid_moves
-    winner = []
-    wins = [ [1,2,3], [4,5,6], [7,8,9], [1,5,9], [3,5,7], [1,4,7], [3,6,9] ]
-    winner = wins.each do |win|
-      win.each_with_index do |n, idx|
-        moves.each do |move|
-          win[idx] = "WIN" if move == n
-        end
-      end
-    end
+    winner = winning_moves(id)
     winner.each do |w|
-      if w.all?("WIN")
+      if w.all?('WIN')
         puts "#{@players[id].id} Wins the game"
         return true
       end
@@ -74,10 +68,21 @@ class Game
     false
   end
 
+  def winning_moves(id)
+    moves = @players[id].valid_moves
+    wins = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 5, 9], [3, 5, 7], [1, 4, 7], [3, 6, 9]]
+    wins.each do |win|
+      win.each_with_index do |n, idx|
+        moves.each do |move|
+          win[idx] = 'WIN' if move == n
+        end
+      end
+    end
+  end
 end
 
+# Player
 class Player
-
   def initialize(id)
     @id = id
     @moves = []
@@ -87,17 +92,18 @@ class Player
 
   def player_play
     player_id_turn
-    get_location
+    obtain_location
   end
 
   def player_id_turn
     print "#{@id} turn to play: \n"
   end
 
-  def get_location
-    loop do 
+  def obtain_location
+    loop do
       location = gets.chomp.to_i
       return location if valid_input(location) && valid_move(location)
+
       print "error please enter a valid location for your move!\n\n"
     end
   end
@@ -108,11 +114,10 @@ class Player
 
   def valid_move(move)
     return true unless @moves.include?(move)
-    return false
-  end
 
+    false
+  end
 end
 
 tictactoe = Game.new
-
 tictactoe.play_game
