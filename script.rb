ad6f78743609498c836eee494305d2ebf634f264
aleksandar-code <aca.jevdjevic@gmail.com>
2022-10-24 08:52:25 +0200

    Create board and players

--- script.rb
+++ script.rb
@@ -1,5 +1,22 @@
-initial_board = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
+
+class Game
+    def initialize
+        @board = Array.new(3) { Array.new(3, "*") }
+        @players = [Player.new(1), Player.new(2)]
+    end
+    attr_accessor :board
+
+    def print_board
+        @board.each { |x| print "#{x}\n" }
+    end
+end
+
+class Player
+    def initialize(id)
+        @id = id
+    end
+end
+
+tictactoe = Game.new
+
+tictactoe.print_board
