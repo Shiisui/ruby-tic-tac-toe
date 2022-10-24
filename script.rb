cc9f6017c59658236e34896beedcfc141926e8e9
aleksandar-code <aca.jevdjevic@gmail.com>
2022-10-24 09:49:58 +0200

    Create play_game

--- script.rb
+++ script.rb
@@ -6,17 +6,43 @@ class Game
         [4, 5, 6],
         [7, 8, 9]
     ]
-    @players = [Player.new(1), Player.new(2)]
+    @players = [Player.new(0), Player.new(1)]
   end
   attr_accessor :board, :players
+
+  
+  def play_game
+    id = 0
+    print_board    
+    loop do
+      @players[id].player_play
+      
+    end
+    
+  end
 
   def print_board
     @board.each { |x| print "#{x}\n" }
     print "To play the game you have to enter the coordinates of the move, example for the top left corner: 1\n\n"
   end
 
-  def play_game
-    
+  def place_at(loc, form)
+    # return X or O using location map to translate the location given by the human.
+
+  end
+
+  def player_switch(id)
+    return 1 if id == 0
+    return 0
+  end
+
+  def player_form(id)
+    return 'X' if id == 0
+    return 'O'
+  end 
+
+  def location_map
+
   end
 
   def check_win
@@ -32,36 +58,21 @@ class Player
   end
   attr_accessor :id
 
-  def player_id_turn
-    print "#{@id} turn to play: \n"
-  end
-
   def player_play
     player_id_turn
-    place_at(get_location, player_form)
+    get_location
+  end
 
+  def player_id_turn
+    print "#{@id} turn to play: \n"
   end
 
   def get_location
     location = gets.chomp
   end
 
-  def place_at(loc, form)
-    # place X or O at location using location map to translate the location given by the human.
-
-  end
-
-  def player_form
-    return 'X' if @id == 1
-    return 'O'
-  end
-
-  def location_map
-
-  end 
-
 end
 
 tictactoe = Game.new
 
-tictactoe.print_board
+tictactoe.play_game
