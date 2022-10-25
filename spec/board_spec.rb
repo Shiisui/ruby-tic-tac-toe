17338c2a233b9791574d6c7c149a17376709f243
aleksandar-code <aca.jevdjevic@gmail.com>
2022-10-25 14:30:01 +0200

    Update spec files

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,3 +1,7 @@
 # frozen_string_literal: true
 
 require_relative '../lib/board'
+
+RSpec.describe Board do
+  
+end
--- spec/players_spec.rb
+++ spec/players_spec.rb
@@ -1,3 +1,7 @@
 # frozen_string_literal: true
 
 require_relative '../lib/players'
+
+RSpec.describe Players do
+  
+end
