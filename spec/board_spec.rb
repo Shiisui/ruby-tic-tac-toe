3815d2219e28401ea840f68cbb667523474e6b6e
aleksandar-code <aca.jevdjevic@gmail.com>
2022-10-25 14:55:28 +0200

    Fix test description

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe Board do
     context 'when a location and a symbol is given' do
       subject(:board_test) { described_class.new }
 
-      it 'Change the board observable state' do
+      it 'change the board observable state' do
         expect { board_test.place_at(1, "X") }.to change { board_test.board.first.first }.to("X")
       end
 
