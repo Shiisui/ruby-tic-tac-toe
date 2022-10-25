fc933037299c2f867858e261f104a0d53a3a8b9f
aleksandar-code <aca.jevdjevic@gmail.com>
2022-10-25 15:24:55 +0200

    Create add_moves test

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -4,14 +4,27 @@ require_relative '../lib/board'
 
 RSpec.describe Board do
 
+subject(:board_test) { described_class.new }
+
   describe '#place_at' do
 
-    context 'when a location and a symbol is given' do
-      subject(:board_test) { described_class.new }
+    context 'when a location and a symbol are given' do
 
       it 'change the board observable state' do
         expect { board_test.place_at(1, "X") }.to change { board_test.board.first.first }.to("X")
       end
+
+    end
+
+  end
+
+  describe '#add_moves' do
+
+    context 'when an id and location are given' do
+
+        it 'change the player valid_moves observable state' do
+          expect { board_test.add_moves(0, 9) }.to change { board_test.players[0].valid_moves }
+        end
 
     end
 
