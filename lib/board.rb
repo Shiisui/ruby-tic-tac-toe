1891a6aa8d781eea70ceea15ac86a7ff5ced8417
aleksandar-code <aca.jevdjevic@gmail.com>
2022-10-25 12:39:57 +0200

    Update class Board

--- lib/board.rb
+++ lib/board.rb
@@ -8,4 +8,23 @@ class Board
         [7, 8, 9]
       ]
   end
+
+  def place_at(loc, form) 
+    @board = @board.each do |x|
+      x.each_with_index do |y, idx|
+        x[idx] = form if y == loc
+      end
+    end
+  end
+
+  def add_moves(id, loc) 
+    @players[0].moves << loc
+    @players[1].moves << loc
+    @players[id].valid_moves << loc
+  end
+
+  def print_board 
+    @board.each { |x| print "#{x}\n" }
+    print "To play the game you have to enter the coordinates of the move, example for the top left corner: 1\n\n"
+  end
 end
--- lib/game.rb
+++ lib/game.rb
@@ -25,25 +25,6 @@ class Game
     end
   end
 
-  def place_at(loc, form)
-    @board = @board.each do |x|
-      x.each_with_index do |y, idx|
-        x[idx] = form if y == loc
-      end
-    end
-  end
-
-  def add_moves(id, loc)
-    @players[0].moves << loc
-    @players[1].moves << loc
-    @players[id].valid_moves << loc
-  end
-
-  def print_board
-    @board.each { |x| print "#{x}\n" }
-    print "To play the game you have to enter the coordinates of the move, example for the top left corner: 1\n\n"
-  end
-
   def player_switch(id)
     return 1 if id.zero?
 
