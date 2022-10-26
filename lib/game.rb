dc0463ddf231cc32147ad929b96145a2bc074bf3
aleksandar-code <aca.jevdjevic@gmail.com>
2022-10-26 13:19:12 +0200

    Fix wins

--- lib/game.rb
+++ lib/game.rb
@@ -49,7 +49,7 @@ class Game
 
   def winning_moves(id)
     moves = @players[id].valid_moves
-    wins = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 5, 9], [3, 5, 7], [1, 4, 7], [3, 6, 9]]
+    wins = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 5, 9], [3, 5, 7], [1, 4, 7], [2, 5, 8], [3, 6, 9]]
     wins.each do |win|
       win.each_with_index do |n, idx|
         moves.each do |move|
